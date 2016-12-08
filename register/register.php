fd4388a105aec7a1c58e5bdc0be3dd5026faa1b3
Diogo Campos <d.diogocampos@gmail.com>
2016-12-08 17:19:25 +0000

    Creats new users and logs in

--- register/register.php
+++ register/register.php
@@ -4,21 +4,15 @@ $db = new PDO('sqlite:../Database/dataBase.db');
 if (isset($_POST["user"])) {
     $user = str_replace(" ", "", $_POST["user"]);
     $pass = str_replace(" ", "", $_POST["pass"]);
+    $email = str_replace(" ", "", $_POST["email"]);
 
     echo $user;
     echo $pass;
 
     include_once('../Database/Connect.php');
 
-    $stmt = $db->prepare("SELECT * FROM Users WHERE usr='".$user."' AND pass='".$pass."'");
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-
-    if (count($result) != 1) {
-        echo "\nLogin nao feito.\n";
-    } else {
-        echo "\nLogin feito.\n";
-    }
+    $stmt = $db->prepare("INSERT INTO Users VALUES ('".$user."','".$pass."','".$email."')");
+    $result = $stmt->execute();
 }
 
 ?>
