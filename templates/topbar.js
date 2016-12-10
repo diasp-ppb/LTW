fa5d39ce9ca51e943790d997e28d414b8b48455a
Diogo Campos <d.diogocampos@gmail.com>
2016-12-10 18:44:56 +0000

    Added login/register box to topbar

--- templates/topbar.js
+++ templates/topbar.js
@@ -1,5 +1,50 @@
-function myFunction(){
-    document.getElementById("topbar-dropdown").classList.toggle("show");
+function btnHandler() {
+    var user;
+
+    $.ajax({
+        url: '../templates/getActiveUser.php',
+        type: 'get',
+        async: false,
+        success: function(data) {
+            user = data;
+        }
+    });
+
+    if (user) {
+        document.getElementById("topbar-dropdown").classList.toggle("show");
+    } else {
+        $("#login-box").fadeIn(300);
+        $('body').append('<div id="mask"></div>');
+        $('#mask').fadeIn(300);
+
+        $('#mask').on('click', function() {
+            $('#mask, #login-box').fadeOut(300, function() {
+                $('#mask').remove();
+            });
+            return false;
+        });
+    }
+}
+
+
+function boxHandler(event) {
+    var boxOptions = document.getElementsByClassName("box-option");
+    for (var i = 0; i < boxOptions.length; i++) {
+        boxOptions[i].className = boxOptions[i].className.replace(" active", "");
+    }
+
+    event.target.className += " active";
+
+    var login = document.getElementById("login");
+    var register = document.getElementById("register");
+
+    if (event.target.innerText === "Login") {
+        register.style.display = "none";
+        login.style.display = "block";
+    } else {
+        register.style.display = "block";
+        login.style.display = "none";
+    }
 }
 
 window.onclick = function(event) {
@@ -14,3 +59,41 @@ window.onclick = function(event) {
         }
     }
 }
+
+$(document).ready(function() {
+    $('#register-form').submit(function() {
+        var user = $.trim($('#register-form input[name="user"]').val());
+        var pass = $.trim($('#register-form input[name="pass"]').val());
+        var confirm = $.trim($('#register-form input[name="confirm"]').val());
+
+        if (user === '') {
+            alert('Please write a username.');
+            return false;
+        }
+
+        if (pass === '') {
+            alert('Please write a password.');
+            return false;
+        }
+
+        if (pass !== confirm) {
+            alert('Password does not match the confirmation field.');
+            return false;
+        }
+    });
+
+    $('#login-form').submit(function() {
+        var user = $.trim($('#login-form input[name="user"]').val());
+        var pass = $.trim($('#login-form input[name="pass"]').val());
+
+        if (user === '') {
+            alert('Please write a username.');
+            return false;
+        }
+
+        if (pass === '') {
+            alert('Please write a password.');
+            return false;
+        }
+    });
+});
--- templates/topbar.php
+++ templates/topbar.php
@@ -1,35 +1,66 @@
 <head>
+    <script
+      src="https://code.jquery.com/jquery-3.1.1.min.js"
+      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+      crossorigin="anonymous"></script>
     <script src="../templates/topbar.js"></script>
     <link rel="stylesheet" href="../templates/topbar.css">
 </head>
 
-<div id="topbar">
-  <div id="topbar-elements">
+<body>
+    <div id="topbar">
+        <div id="topbar-elements">
+            <h1>
+                <a href="../feed/feed.php">Já Comia</a>
+            </h1>
 
-    <h1><a href="../feed/feed.php">Já Comia</a></h1>
+            <div id="searchbar">
+                <form action="../feed/feed.php" method="get">
+                    <input type="text" name="search" placeholder="Search">
+                </form>
+            </div>
 
-    <div id="searchbar">
-        <form action="../feed/feed.php" method="get">
-            <input type="text" name="search" placeholder="Search">
-        </form>
+            <div id="topbar-login">
+                <button onclick="btnHandler()" class="dropbtn">
+                  <?php
+                    session_start();
+                    if (isset($_SESSION["user"])) {
+                        echo $_SESSION["user"];
+                    } else {
+                        echo "Login/Registar";
+                    }
+                    ?>
+                </button>
+
+                <div id="topbar-dropdown" class="dropdown-content">
+                  <a href="#">My Profile</a>
+                  <a href="../newRestaurant/newRestaurant.php">Create Restaurant</a>
+                  <a href="../login/logout.php">Log Out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="login-box" class="box">
+        <ul class="box-select">
+            <li class="box-option" onClick="boxHandler(event)">Login</li>
+            <li class="box-option" onClick="boxHandler(event)">Registar</li>
+        </ul>
+        <div id="login">
+            <form id="login-form" action="../login/login.php" method="POST">
+                <input type="text" name="user" placeholder="Username" />
+                <input type="password" name="pass" placeholder="Password" />
+                <input type="submit" value="Login"/>
+            </form>
+        </div>
+        <div id="register">
+            <form id="register-form" action="../register/register.php" method="POST">
+                <input type="text" name="user" placeholder="Username"/>
+                <input type="password" name="pass" placeholder="Password"/>
+                <input type="password" name="confirm" placeholder="Confirm your password"/>
+                <input type="email" name="email" placeholder="Email"/>
+                <input type="submit" value="Register"/>
+            </form>
+        </div>
     </div>
 
-    <div id="topbar-login">
-
-      <button onclick="myFunction()" class="dropbtn">
-          <?php
-            session_start();
-            echo $_SESSION["user"];
-            ?>
-      </button>
-      <div id="topbar-dropdown" class="dropdown-content">
-          <a href="#">My Profile</a>
-          <a href="../newRestaurant/newRestaurant.php">Create Restaurant</a>
-          <a href="#">Log Out</a>
-      </div>
-    </div>
-
-
-
-  </div>
-</div>
+</body>
