a0aa12dde4230d03bcd13ab55332017103c4b913
Diogo Campos <d.diogocampos@gmail.com>
2016-12-14 03:57:56 +0000

    Refactored submitReview

---
+++ reviewRestaurant/submitReview.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_POST['Submit'])) {
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $comment = $_POST['comment'];
+    $classification = $_POST['classification'];
+
+    include_once '../Database/Connect.php';
+    include_once '../templates/getUserID.php';
+
+    $userid =  getUserID();
+
+    $insert = $db->prepare("INSERT INTO Reviews (userID, restaurant, title, opinion, classification)
+                            VALUES ('$userid','$id','$title','$comment','$classification');");
+
+    try {
+        $insert->execute();
+    } catch (PDOException $e) {
+    }
+
+    header('Location: ../restaurant/restaurant.php?id='.$id);
+}
